4c60ceb1a3fff081b4d14729df84aaab4afd2352
Abdoahmed88 <111208239+Abdoahmed88@users.noreply.github.com>
2023-11-09 13:57:14 -0800

    Add files via upload

---
+++ task1 by if/task1 by if/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2,sum;
+            string op;
+            Console.Write("Enter The First Numper : ");
+            num1 =Convert.ToInt16( Console.ReadLine());
+            Console.Write("Enter The second Numper : ");
+            num2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("choose Arithmetic operators + , - , * , / ");
+            op = Console.ReadLine();
+            if (op=="+")
+            {
+                sum = num1 + num2;
+                Console.Write("the sum is :" + sum);
+            }
+            else if (op=="-")
+            {
+                sum = num1 - num2;
+                Console.Write("the sum is :" + sum);
+            }
+            else if (op == "*")
+            {
+                sum = num1 * num2;
+                Console.Write("the sum is :" + sum);
+            }
+            else if (op == "/")
+            {
+                sum = num1 / num2;
+                Console.Write("the sum is :" + sum);
+            }
+
+        }
+    }
+}
---
+++ task2 by switch/task2 by switch/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2,sum;
+            string op;
+            Console.Write("Enter the first numper :");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("choose Arithmetic operators + , - , * , / ");
+            op = Console.ReadLine();
+            Console.Write("Enter the second numper :");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            switch (op)
+            {
+                case "+":
+                    sum =num1 + num2;
+                    Console.WriteLine("the sum is :\a" + sum);
+                    break;
+                case "-":
+                    sum = num1 - num2;
+                    Console.WriteLine("the sum is :\a" + sum);
+                    break;
+                case "*":
+                    sum = num1 * num2;
+                    Console.WriteLine("the sum is :\a" + sum);
+                    break;
+                case "/":
+                    sum = num1 / num2;
+                    Console.WriteLine("the sum is :\a" + sum);
+                    break;
+
+
+
+            }
+        }
+    }
+}
